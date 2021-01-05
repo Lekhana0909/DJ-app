278443da059470af06efa0f57330cc504d069335
Lekhana0909 <74604087+Lekhana0909@users.noreply.github.com>
2021-01-05 19:50:17 +0530

    Add files via upload

--- Dj.js
+++ Dj.js
@@ -4,6 +4,7 @@ leftWristX=0;
 leftWristY=0;
 rightwristX=0;
 rightWristY=0;
+scoreLeftWrist=0;
 function preload(){
     song1= loadSound("music.mp3");
     song2= loadSound("music2.mp3");
@@ -23,6 +24,25 @@ function modelLoaded(){
 }
 function draw(){
     image(video, 0, 0, 600, 500);
+    fill("#BD1742");
+    stroke("#BD1742");
+
+    if (song1.isPlaying()){
+    song1.stop();
+    }
+    
+    if (scoreLeftWrist > 0.2){
+        if (song2.isPlaying()){
+            song2.stop();
+        }
+        circle(leftWristX, leftWristY, 20);
+        InNumberLeftWristY= Number(leftWristY);
+        remove_decimals= floor(InNumberLeftWristY);
+        leftWristY_divide_1000= remove_decimals/1000;
+        volume= leftWristY_divide_1000*2;
+        document.getElementById("volume").innerHTML="Volume="+ volume;
+        song1.play();
+    }
 }
 function play(){
     song1.play();
@@ -32,6 +52,8 @@ function play(){
 function gotPoses(results){
     if (results.length>0){
         console.log(results);
+        scoreLeftWrist= results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist="+ scoreLeftWrist);
         leftWristX= results[0].pose.leftWrist.x;
         leftWristY= results[0].pose.leftWrist.y;
         console.log("leftWristX" + leftWristX + "leftWristY" + leftWristY);
